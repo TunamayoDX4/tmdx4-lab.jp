764ac5ba478ee59d1339a85c262bfc5aca55a520
TunamayoDX4 <tsunamayodx4@outlook.jp>
2025-03-14 00:52:06 +0900

    コードの分割とか

--- app/tmdx4-workplace/src/usersys/mod.rs
+++ app/tmdx4-workplace/src/usersys/mod.rs
@@ -1,12 +1,23 @@
 //! ユーザシステムの実装
 
-use argon2::{PasswordHash, password_hash::PasswordHashString};
+use argon2::{PasswordHasher, PasswordVerifier, password_hash::PasswordHashString};
+use rand::{
+  SeedableRng,
+  rngs::{OsRng, ThreadRng},
+};
+use rand_chacha::{ChaCha20Core, ChaCha20Rng};
 use serde::{Deserialize, Serialize};
 use sha3::{Digest, Sha3_256};
 use std::{
+  cell::{LazyCell, RefCell},
   io::{Read, Write},
+  ops::Deref,
   str::FromStr,
 };
+
+thread_local! {
+  static PRNG: LazyCell<RefCell<ChaCha20Rng>> = LazyCell::new(|| RefCell::new(ChaCha20Rng::from_entropy()));
+}
 
 /// ユーザID生成失敗エラー
 #[derive(Debug)]
@@ -20,8 +31,11 @@ pub enum UserDataError {
   /// ユーザデータの書き込み時のエラー
   UserDataSaveError(std::io::Error),
 
-  /// パスワードハッシュの読み取り時のエラー
-  PasswordHashReadError(argon2::password_hash::Error),
+  /// パスワードハッシュのエラー
+  PasswordHashError(argon2::password_hash::Error),
+
+  /// Argon2におけるハッシュ生成に関するエラー
+  Argon2Error(argon2::Error),
 
   /// MsgPackのデコードのエラー
   MPackDecodeError(rmp_serde::decode::Error),
@@ -38,9 +52,8 @@ impl std::fmt::Display for UserDataError {
       Self::InvalidCharInIdent => f.write_str("Invalid charactor with in userid"),
       Self::UserDataLoadError(e) => f.write_fmt(format_args!("User data loading error: {e}")),
       Self::UserDataSaveError(e) => f.write_fmt(format_args!("User data saving error: {e}")),
-      Self::PasswordHashReadError(e) => {
-        f.write_fmt(format_args!("Password hash reading error: {e}"))
-      }
+      Self::PasswordHashError(e) => f.write_fmt(format_args!("Password hash reading error: {e}")),
+      Self::Argon2Error(e) => f.write_fmt(format_args!("Argon2 hasher error: {e}")),
       Self::MPackDecodeError(e) => f.write_fmt(format_args!("Message pack decode error: {e}")),
       Self::MPackEncodeError(e) => f.write_fmt(format_args!("Message pack encode error: {e}")),
       Self::LogicError(error) => f.write_fmt(format_args!(
@@ -55,6 +68,7 @@ impl std::fmt::Display for UserDataError {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct UserIdent(#[serde(with = "serde_bytes")] [u8; 32]);
 impl UserIdent {
+  /// ユーザ名にこの文字入れたらNG(ファイル名に使うので…)
   pub const INVALID_CHARS: &'static [char] =
     &['/', '\\', '*', '+', '?', '.', ',', '~', '^', '<', '>'];
 
@@ -72,20 +86,132 @@ impl UserIdent {
   }
 }
 
+/// ユーザデータについてのコンフィグ
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UserDataConfig {
+  /// セキュリティ関連データのパス
+  pub sec_data_path: String,
+
+  /// 各ユーザ用拡張データのパス
+  pub user_data_path: String,
+
+  /// Argon2のメモリコスト
+  pub argon2_m_cost: u32,
+
+  /// Argon2の時間的コスト
+  pub argon2_t_cost: u32,
+
+  /// Argon2の並列化コスト
+  pub argon2_p_cost: u32,
+}
+impl UserDataConfig {
+  pub fn init_argon2_param(&self) -> Result<argon2::Params, argon2::Error> {
+    argon2::Params::new(
+      self.argon2_m_cost,
+      self.argon2_t_cost,
+      self.argon2_p_cost,
+      None,
+    )
+  }
+}
+impl Default for UserDataConfig {
+  fn default() -> Self {
+    Self {
+      sec_data_path: "./user_data/secure".into(),
+      user_data_path: "./user_data/user_data".into(),
+      argon2_m_cost: 4096,
+      argon2_t_cost: 1,
+      argon2_p_cost: 2,
+    }
+  }
+}
+
 /// ユーザデータ
-pub struct UserData {
+pub struct UserData<D>
+where
+  D: Serialize + for<'de> Deserialize<'de> + Send + Sync + Sized,
+{
   ident: UserIdent,
   pswd_hash: PasswordHashString,
+  user_data: D,
 }
-impl UserData {
-  pub fn load(name: &str, load_dir: &str) -> Result<Self, UserDataError> {
+impl<D> UserData<D>
+where
+  D: Serialize + for<'de> Deserialize<'de> + Send + Sync + Sized,
+{
+  pub fn new(
+    name: &str,
+    pswd: &str,
+    user_data: D,
+    configure: &UserDataConfig,
+  ) -> Result<Self, UserDataError> {
+    let ident = UserIdent::generate(name)?;
+    let argon2 = argon2::Argon2::new(
+      argon2::Algorithm::Argon2id,
+      argon2::Version::V0x13,
+      configure
+        .init_argon2_param()
+        .map_err(|e| UserDataError::Argon2Error(e))?,
+    );
+    let salt =
+      PRNG.with(|prng| argon2::password_hash::SaltString::generate(&mut *prng.borrow_mut()));
+    let hash = argon2
+      .hash_password(pswd.trim().as_bytes(), salt.as_salt())
+      .map_err(|e| UserDataError::PasswordHashError(e))?;
+    let hash = PasswordHashString::from(hash);
+    Ok(Self {
+      ident,
+      pswd_hash: hash,
+      user_data,
+    })
+  }
+  pub fn verify(
+    &mut self,
+    pswd: &str,
+    new_pswd: Option<&str>,
+    new_data: Option<D>,
+    configure: &UserDataConfig,
+  ) -> Result<bool, UserDataError> {
+    let pb = pswd.trim().as_bytes();
+    let argon2 = argon2::Argon2::new(
+      argon2::Algorithm::Argon2id,
+      argon2::Version::V0x13,
+      configure
+        .init_argon2_param()
+        .map_err(|e| UserDataError::Argon2Error(e))?,
+    );
+    match argon2.verify_password(pb, &self.pswd_hash.password_hash()) {
+      Ok(_) => {}
+      Err(argon2::password_hash::Error::Password) => return Ok(false),
+      Err(e) => return Err(UserDataError::PasswordHashError(e)),
+    }
+    let salt =
+      PRNG.with(|prng| argon2::password_hash::SaltString::generate(&mut *prng.borrow_mut()));
+    let hash = argon2
+      .hash_password(
+        new_pswd.map(|np| np.trim().as_bytes()).unwrap_or(pb),
+        salt.as_salt(),
+      )
+      .map_err(|e| UserDataError::PasswordHashError(e))?;
+    self.pswd_hash = PasswordHashString::from(hash);
+    if let Some(nd) = new_data {
+      self.user_data = nd
+    }
+    self.save(&configure)?;
+    Ok(true)
+  }
+
+  pub fn load(name: &str, configure: &UserDataConfig) -> Result<Self, UserDataError> {
     let ident = match UserIdent::generate(name) {
       Ok(ident) => ident,
       Err(e) => return Err(e),
     };
-    let mut buffer = String::with_capacity(load_dir.len() + 21);
-    buffer += load_dir;
+    let mut buffer = String::with_capacity(configure.sec_data_path.len() + 21);
+    let mut buffer2 = String::with_capacity(configure.user_data_path.len() + 21);
+    buffer += &configure.sec_data_path;
+    buffer2 += &configure.user_data_path;
     buffer.push('/');
+    buffer2.push('/');
     for ch in ident
       .0
       .iter()
@@ -93,8 +219,10 @@ impl UserData {
       .filter_map(|b| char::from_digit(b as u32, 16))
     {
       buffer.push(ch);
+      buffer2.push(ch);
     }
     buffer += ".bin";
+    buffer2 += ".bin";
     let mut rdr = std::io::BufReader::new(
       std::fs::File::open(&buffer).map_err(|e| UserDataError::UserDataLoadError(e))?,
     );
@@ -103,26 +231,55 @@ impl UserData {
       .read_to_string(&mut buffer)
       .map_err(|e| UserDataError::UserDataLoadError(e))?;
     let pswd_hash =
-      PasswordHashString::new(&buffer).map_err(|e| UserDataError::PasswordHashReadError(e))?;
+      PasswordHashString::new(&buffer).map_err(|e| UserDataError::PasswordHashError(e))?;
+    let rdr = std::io::BufReader::new(
+      std::fs::File::open(&buffer2).map_err(|e| UserDataError::UserDataLoadError(e))?,
+    );
+    let user_data = rmp_serde::from_read(rdr).map_err(|e| UserDataError::MPackDecodeError(e))?;
 
-    Ok(Self { ident, pswd_hash })
+    Ok(Self {
+      ident,
+      pswd_hash,
+      user_data,
+    })
   }
-  pub fn save(&self, load_dir: &str) -> Result<(), UserDataError> {
-    if !std::fs::exists(load_dir).map_err(|e| UserDataError::UserDataSaveError(e))? {
-      std::fs::create_dir_all(load_dir).map_err(|e| UserDataError::UserDataSaveError(e))?
+
+  pub fn save(&self, configure: &UserDataConfig) -> Result<(), UserDataError> {
+    if !std::fs::exists(&configure.sec_data_path)
+      .map_err(|e| UserDataError::UserDataSaveError(e))?
+    {
+      std::fs::create_dir_all(&configure.sec_data_path)
+        .map_err(|e| UserDataError::UserDataSaveError(e))?
     }
-    let mut buffer = String::with_capacity(load_dir.len() + 21);
-    buffer += load_dir;
+    let mut buffer = String::with_capacity(&configure.sec_data_path.len() + 21);
+    let mut buffer2 = String::with_capacity(&configure.user_data_path.len() + 21);
+    buffer += &configure.sec_data_path;
+    buffer2 += &configure.user_data_path;
     buffer.push('/');
+    buffer2.push('/');
     for ch in self
       .ident
       .0
       .iter()
-      .flat_map(|b| [b & 0xF0, b & 0xF0].into_iter())
+      .flat_map(|b| [b & 0xF0, b & 0x0F].into_iter())
       .filter_map(|b| char::from_digit(b as u32, 16))
     {
       buffer.push(ch);
+      buffer2.push(ch);
     }
-    todo!()
+    buffer += ".bin";
+    buffer2 += ".bin";
+    let mut wrt = std::io::BufWriter::new(
+      std::fs::File::create(buffer).map_err(|e| UserDataError::UserDataSaveError(e))?,
+    );
+    wrt
+      .write(self.pswd_hash.as_str().as_bytes())
+      .map_err(|e| UserDataError::UserDataSaveError(e))?;
+    let mut wrt = std::io::BufWriter::new(
+      std::fs::File::create(buffer2).map_err(|e| UserDataError::UserDataSaveError(e))?,
+    );
+    rmp_serde::encode::write(&mut wrt, &self.user_data)
+      .map_err(|e| UserDataError::MPackEncodeError(e))?;
+    Ok(())
   }
 }
